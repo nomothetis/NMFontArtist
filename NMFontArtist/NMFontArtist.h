90f5ee43f96b56554c8a3cef69ec78894596dec6
Alexandros Salazar <nomothetis@gmail.com>
2012-07-12 21:04:19 -0400

    Initial Commit

---
+++ NMFontArtist/NMFontArtist.h
@@ -0,0 +1,13 @@
+//
+//  NMFontArtist.h
+//  NMFontArtist
+//
+//  Created by Salazar, Alexandros on 7/12/12.
+//  Copyright (c) 2012 nomothetis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NMFontArtist : NSObject
+
+@end
---
+++ NMFontArtist/NMFontArtist.m
@@ -0,0 +1,13 @@
+//
+//  NMFontArtist.m
+//  NMFontArtist
+//
+//  Created by Salazar, Alexandros on 7/12/12.
+//  Copyright (c) 2012 nomothetis. All rights reserved.
+//
+
+#import "NMFontArtist.h"
+
+@implementation NMFontArtist
+
+@end
